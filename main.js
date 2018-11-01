60dcee3891555125ee0912b9be72bc1d502601b4
Marcin <mboczkowski2@gmail.com>
2018-11-01 19:53:05 +0100

    Another way

--- main.js
+++ main.js
@@ -1,37 +1,26 @@
 const spanText = document.querySelector(".text");
 const spnCursor = document.querySelector(".cursor");
-let indexLetter = 0;
 const txt = ['Tekst numer 1...', 'Następny tekst...', 'Ostatni tekst...'];
-let indexText = 0;
+let activeLetter = -15;
+let activeText = 0;
 
 
 
 const addLetter = () => {
-  let activeText = txt[indexText];
-  const printLetters = () => {
-    spanText.textContent += activeText[indexLetter];
-    indexLetter++;
-    if (indexLetter === activeText.length) {
-      clearInterval(interval);
-      setTimeout(() => {
-        indexText++;
-        if (indexText != txt.length) {
-          spanText.textContent = "";
-          indexLetter = 0;
-          addLetter();
-        } else return;
-        // setTimeout(() => {
-        //   indexText = 0;
-        //   indexLetter = 0;
-        //   spanText.textContent = "";
-        //   addLetter();
-        // }, 2000);
-
-      }, 1000);
-    }
-  };
-  const interval = setInterval(printLetters, 100);
-
+  if (activeLetter >= 0) {
+    spanText.textContent += txt[activeText][activeLetter];
+  }
+  activeLetter++;
+  if (activeLetter === txt[activeText].length) {
+    activeText++;
+    if (activeText === txt.length) return;
+    return setTimeout(() => {
+      activeLetter = -15;
+      spanText.textContent = "";
+      addLetter();
+    }, 2000);
+  }
+  setTimeout(addLetter, 100);
 };
 
 
